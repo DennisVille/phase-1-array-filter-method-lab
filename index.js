917b5c9492a20e696377e843619808cdcde30e79
dennis <denismukuria@gmail.com>
2024-12-31 23:12:53 +0300

    add 3 filter methods

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(arr, name){
+    //const result = arr.filter((n) => n.toLowerCase().includes(name.toLowerCase()));
+    const result = arr.filter((n) => n.toLowerCase() == name.toLowerCase());
+    return result; 
+}
+function fuzzyMatch(arr, name){
+    const result = arr.filter((n) => n.startsWith(name));
+    return result;
+}
+function matchName(arr, driver){
+    const result = arr.filter((el) => el.name == driver);
+    return result;
+}
